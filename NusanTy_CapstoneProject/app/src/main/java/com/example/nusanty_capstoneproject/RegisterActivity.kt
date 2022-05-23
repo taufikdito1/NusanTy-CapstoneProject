abe723ef1fd735d3408ecd5fe9ea9a987f33d70b
Taufik Dito Wijaya <taufikdito1@gmail.com>
2022-05-23 13:26:52 +0700

    Add Register Activity
    Menambahkan Register Activity

---
+++ NusanTy_CapstoneProject/app/src/main/java/com/example/nusanty_capstoneproject/RegisterActivity.kt
@@ -0,0 +1,11 @@
+package com.example.nusanty_capstoneproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
