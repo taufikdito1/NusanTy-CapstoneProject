5f57badc53c69e037855bd6c8a5b54dd08689c79
Taufik Dito Wijaya <taufikdito1@gmail.com>
2022-05-23 13:20:35 +0700

    Add Login Activity
    Menambahkan Login Acivity

---
+++ NusanTy_CapstoneProject/app/src/main/java/com/example/nusanty_capstoneproject/LoginActivity.kt
@@ -0,0 +1,11 @@
+package com.example.nusanty_capstoneproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
